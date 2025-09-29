9a4bf860c65cd88fe0b51cf1b407c2fc46ce6b13
akbar34 <akbartamar34@gmail.com>
2025-09-29 07:57:36 +0700

    benerin callbackLagi

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,22 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        pathname: '/**',  // This means any path under the domain will be allowed
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**", // This means any path under the domain will be allowed
       },
-
-      
     ],
   },
   env: {
-    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
     KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}`,
     KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
-  }
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}/auth-callback`,
+  },
 }
 
 export default nextConfig
