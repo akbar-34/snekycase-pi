112d30c564f77c35cfd2a2e5ca033e4be9452f37
akbar34 <akbartamar34@gmail.com>
2025-09-29 07:33:02 +0700

    benerin callback

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,17 @@ const nextConfig = {
         hostname: 'utfs.io',
         pathname: '/**',  // This means any path under the domain will be allowed
       },
+
+      
     ],
   },
+  env: {
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
+  }
 }
 
 export default nextConfig
